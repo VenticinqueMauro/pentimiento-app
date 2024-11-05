78466f8c7baccec5d0c4830607f71ed825dbfa4b
Mauro Venticinque <mauro.venticinque@hotmail.com>
2024-11-05 19:24:56 -0300

    test 2 middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-    // Manejo de solicitudes OPTIONS para permitir CORS
+    // Manejo de solicitudes OPTIONS para permitir CORS en todas las rutas
     if (request.method === 'OPTIONS') {
         const response = new NextResponse(null, { status: 204 });
         response.headers.set('Access-Control-Allow-Origin', '*');
@@ -12,26 +12,31 @@ export async function middleware(request: NextRequest) {
         return response;
     }
 
-    // Verificación del JWT para otras solicitudes
-    const token = request.cookies.get('pentimiento_app')?.value;
+    // Aplica autenticación solo en la ruta de `/dashboard`
+    if (request.nextUrl.pathname.startsWith('/dashboard')) {
+        const token = request.cookies.get('pentimiento_app')?.value;
 
-    if (token === undefined) {
-        return NextResponse.redirect(new URL('/auth/login', request.url));
+        if (token === undefined) {
+            return NextResponse.redirect(new URL('/auth/login', request.url));
+        }
+
+        try {
+            await jwtVerify(token, new TextEncoder().encode(`${process.env.JWT_KEY}`));
+            return NextResponse.next();
+        } catch (error) {
+            console.log(error);
+            return NextResponse.redirect(new URL('/', request.url));
+        }
     }
 
-    try {
-        await jwtVerify(token, new TextEncoder().encode(`${process.env.JWT_KEY}`));
-        return NextResponse.next();
-    } catch (error) {
-        console.log(error);
-        return NextResponse.redirect(new URL('/', request.url));
-    }
+    // Permitir el acceso a otras rutas sin autenticación
+    return NextResponse.next();
 }
 
-// Configuración del middleware para aplicar en todas las rutas necesarias
+// Configuración para aplicar el middleware solo en rutas específicas
 export const config = {
     matcher: [
-        '/dashboard/:path*',
-        '/portfolio/:path*', // Añadido para asegurar que también cubre las rutas de "portfolio"
+        '/dashboard/:path*', // Requiere autenticación
+        '/portfolio/:path*', // No requiere autenticación, pero maneja OPTIONS
     ],
 };
