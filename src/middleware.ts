9cfab9d239310b06be4cb273bac446aae95393a1
Mauro Venticinque <mauro.venticinque@hotmail.com>
2024-11-05 19:21:57 -0300

    test middleware cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,35 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
+    // Manejo de solicitudes OPTIONS para permitir CORS
+    if (request.method === 'OPTIONS') {
+        const response = new NextResponse(null, { status: 204 });
+        response.headers.set('Access-Control-Allow-Origin', '*');
+        response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+        return response;
+    }
 
+    // Verificación del JWT para otras solicitudes
     const token = request.cookies.get('pentimiento_app')?.value;
 
-    if (token === undefined) return NextResponse.redirect(new URL('/auth/login', request.url));
+    if (token === undefined) {
+        return NextResponse.redirect(new URL('/auth/login', request.url));
+    }
 
     try {
-        await jwtVerify(token, new TextEncoder().encode(`${process.env.JWT_KEY}`))
-        return NextResponse.next()
+        await jwtVerify(token, new TextEncoder().encode(`${process.env.JWT_KEY}`));
+        return NextResponse.next();
     } catch (error) {
-        console.log(error)
-        return NextResponse.redirect(new URL('/', request.url))
+        console.log(error);
+        return NextResponse.redirect(new URL('/', request.url));
     }
 }
 
+// Configuración del middleware para aplicar en todas las rutas necesarias
 export const config = {
     matcher: [
-        '/dashboard/:path*'
+        '/dashboard/:path*',
+        '/portfolio/:path*', // Añadido para asegurar que también cubre las rutas de "portfolio"
     ],
-};  
+};
