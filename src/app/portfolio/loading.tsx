d554e2d7b5f23991788eb9369183d62148545816
Mauro Venticinque <mauro.venticinque@hotmail.com>
2024-11-06 06:02:07 -0300

    reorganizacion de serverPage portfolio

---
+++ src/app/portfolio/loading.tsx
@@ -0,0 +1,22 @@
+
+export default function LoadingPortfolio() {
+    return (
+        <div >
+            <div className="w-40 bg-gray-200 my-8 animate-puls mx-auto py-4 rounded"></div>
+            <div className="flex justify-center space-x-4 mb-8">
+                {Array.from({ length: 4 }).map((_, i) => (
+                    <div key={i} className="w-[90px] h-[36px] bg-gray-200 rounded mb-2 animate-puls"></div>
+                ))}
+            </div>
+            <div className="grid grid-cols-[repeat(auto-fill,_minmax(400px,_1fr))] gap-2">
+                {Array.from({ length: 20 }).map((_, i) => (
+                    <div key={i} className="flex flex-col items-center justify-center bg-gray-200 animate-pulse">
+                        <div className="w-full h-48"></div>
+                        <div className="w-3/4 h-6"></div>
+                        <div className="w-1/2 h-4"></div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
