8fbc6e4561299a63fca91f5fa5118d30dc567f75
Mauro Venticinque <mauro.venticinque@hotmail.com>
2024-11-14 09:58:49 -0300

    add metadata en /equipo

--- src/app/equipo/jorge/layout.tsx
+++ src/app/equipo/jorge/layout.tsx
@@ -2,8 +2,17 @@ import Footer from "@/components/footer/Footer";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Jorge Russo",
-    description: "Jorge Russo",
+    title: "Jorge Russo - Colorista Senior",
+    description: `Jorge Russo es un colorista Senior con treinta años de experiencia en corrección de color digital para cine, publicidad y TV. Ha trabajado con destacados directores de fotografía y cine en proyectos nacionales e internacionales.`,
+    openGraph: {
+        title: "Jorge Russo - Colorista Senior",
+        description: `Jorge Russo es un colorista Senior con treinta años de experiencia en corrección de color digital para cine, publicidad y TV. Con una trayectoria que incluye cine y publicidad de alta calidad, Jorge aporta una sensibilidad única al proceso creativo.`,
+        images: [{ url: "https://res.cloudinary.com/da305oaa0/image/upload/v1731571870/equipo/qyvcvksg35iat4ivsb4a.png" }],
+        url: "https://pentimento.cc/equipo/jorge",
+        siteName: "Pentimento Color Grading",
+        locale: "es_AR",
+        type: "profile",
+    }
 };
 
 export default function JorgeLayout({ children }: { children: React.ReactNode }) {
--- src/app/equipo/rodrigo/layout.tsx
+++ src/app/equipo/rodrigo/layout.tsx
@@ -2,8 +2,17 @@ import Footer from "@/components/footer/Footer";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Rodrigo Silvestri",
-    description: "Rodrigo Silvestri",
+    title: "Rodrigo Silvestri - Colorista y Supervisor Técnico",
+    description: `Rodrigo Silvestri es colorista y supervisor técnico en Pentimento, con amplia experiencia en color y flujos de trabajo de postproducción en cine y series. Ha trabajado en proyectos destacados como El Ángel y series HDR como IOSI S01 y El Presidente S02.`,
+    openGraph: {
+        title: "Rodrigo Silvestri - Colorista y Supervisor Técnico",
+        description: `Con formación en dirección de fotografía y color, Rodrigo Silvestri ha trabajado en proyectos de cine y series de alto perfil. Actualmente, forma parte del equipo de Pentimento, desarrollando flujos de trabajo avanzados de color y postproducción.`,
+        images: [{ url: "https://res.cloudinary.com/da305oaa0/image/upload/v1731575398/equipo/f78mvvjzdilh58f2jttf.png" }],
+        url: "https://pentimento.cc/equipo/rodrigo",
+        siteName: "Pentimento Color Grading",
+        locale: "es_AR",
+        type: "profile",
+    }
 };
 
 export default function JorgeLayout({ children }: { children: React.ReactNode }) {
